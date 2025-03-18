a94726344f14b7aa9ffa96665129cfd3640c75db
shreshtsharma <sharmashresht7@gmail.com>
2025-03-18 15:27:24 +0530

    connecting mongo db

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// this dotenv syntax will work but this breaks the consistency of our code so we try to use import statement
+// require("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+import express from "express";
+const app = express();
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB();
+
+// we can write this code if we want to connect our db from index.js file or our main file directly
+// which is not a good approach
+/*
+
+// IIFE function
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    app.on("error", (error) => {
+      console.log("Application not able to talk to database", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw error;
+  }
+})();
+
+*/
