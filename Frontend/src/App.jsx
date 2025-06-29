f4635c7ee8ecf89e3be2c315fc22dadd2260fcba
shreshtsharma <sharmashresht7@gmail.com>
2025-06-30 00:06:51 +0530

    project initialization

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-slate-500 w-full h-full text-black text-3xl">
+        Hello
+      </div>
+    </>
+  );
+}
+
+export default App;
